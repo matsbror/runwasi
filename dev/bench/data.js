98dfd5001463eb065d0b3848c16f347c135f5d93
github-action-benchmark <github@users.noreply.github.com>
2026-04-16 02:25:52 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for 93753c7ec8b81aae0620ca77e2d1f22d0b905693

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776222585857,
+  "lastUpdate": 1776306352019,
   "repoUrl": "https://github.com/matsbror/runwasi",
   "entries": {
     "HTTP Throughput": [
@@ -39390,6 +39390,52 @@ window.BENCHMARK_DATA = {
             "value": 279169979,
             "range": "± 7125024",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "93753c7ec8b81aae0620ca77e2d1f22d0b905693",
+          "message": "chore(deps): bump lycheeverse/lychee-action from 2.4.1 to 2.5.0 (#1028)\n\nBumps [lycheeverse/lychee-action](https://github.com/lycheeverse/lychee-action) from 2.4.1 to 2.5.0.\n- [Release notes](https://github.com/lycheeverse/lychee-action/releases)\n- [Commits](https://github.com/lycheeverse/lychee-action/compare/82202e5e9c2f4ef1a55a3d02563e1cb6041e5332...5c4ee84814c983aa7164eaee476f014e53ff3963)\n\n---\nupdated-dependencies:\n- dependency-name: lycheeverse/lychee-action\n  dependency-version: 2.5.0\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-08-19T17:40:04Z",
+          "url": "https://github.com/matsbror/runwasi/commit/93753c7ec8b81aae0620ca77e2d1f22d0b905693"
+        },
+        "date": 1776306345017,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15824,
+            "unit": "kB",
+            "extra": "shim: 12664 kB\nzygote: 3160 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66828,
+            "unit": "kB",
+            "extra": "shim: 54080 kB\nzygote: 12748 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 19528,
+            "unit": "kB",
+            "extra": "shim: 15876 kB\nzygote: 3652 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20172,
+            "unit": "kB",
+            "extra": "shim: 16548 kB\nzygote: 3624 kB"
           }
         ]
       }
